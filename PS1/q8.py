d414cbfe719d50e9d4e6430b1bf9d204392eb6fe
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2021-12-04 12:27:10 -0600

    q8 Node and LL classes created

---
+++ PS1/q8.py
@@ -0,0 +1,20 @@
+#Implement "reverse" as a method of SinglyLinkedList, called as L.reverse(),
+# that reverses the SinglyLinkedList instance L
+# using only a constant amount of additional space and not using any recursion.
+
+
+class Node:
+	def __init__(self, data=0, next=None):
+		self.data = data
+		self.next = Next
+
+
+
+class LinkedList:
+
+	def __init__(self, head=None):
+		self.head = head
+
+
+	def reverse(self):
+		pass

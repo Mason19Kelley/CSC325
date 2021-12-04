5bcea0db080e675ff2ad0624529790f8834ec0fc
j-w-s <69388380+j-w-s@users.noreply.github.com>
2021-12-04 13:38:33 -0600

    Update q5.py

---
+++ PS1/q5.py
@@ -0,0 +1,18 @@
+# no. disks, source, destination, spare
+def hanoi(disks, src, dest, spr):
+    # base case
+    if (disks == 1):
+        print("{}->{}".format(src, dest))
+   # geberal case
+    else:
+        # step 1
+        # move disks-1 from src to spare
+        hanoi(disks-1, src, spr, dest)
+        
+        # step 2
+        # move bottom disk from src to dest
+        hanoi(1, src, dest, spr)
+
+        # step 3
+        # move disks-1 from spare to dest
+        hanoi(disks-1, dest, src, spr)

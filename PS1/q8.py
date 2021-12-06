f4626f1626856824d1fd2af60f9d9d8ea111abc1
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2021-12-06 10:37:53 -0600

    q8 base code added

--- PS1/q8.py
+++ PS1/q8.py
@@ -3,18 +3,64 @@
 # using only a constant amount of additional space and not using any recursion.
 
 
-class Node:
-	def __init__(self, data=0, next=None):
-		self.data = data
-		self.next = Next
+class SinglyLinkedList:
+	class _Node:
+		__slots__ = '_element', '_next'
 
+		def __init__(self, element, next):
+			self._element = element
+			self._next = next
 
+	def __init__(self):
+		self._head = None
+		self._tail = None
+		self._size = 0
 
-class LinkedList:
+	def __len__(self):
+		return self._size
 
-	def __init__(self, head=None):
-		self.head = head
+	def is_empty(self):
+		return self._size == 0
 
+	def add_first(self, e):
+		self._head = self._Node(e, self._head)
+		if self.is_empty():
+			self._tail = self._head
+		self._size += 1
 
-	def reverse(self):
-		pass
+	def add_last(self, e):
+		newnode = self._Node(e, None)
+		if self.is_empty():
+			self._head = newnode
+		else:
+			self._tail._next = newnode
+		self._tail = newnode
+		self._size += 1
+
+	def remove_first(self):
+		if self.is_empty():
+			raise Empty('Linked list is empty')
+		answer = self._head._element
+		self._head = self._head._next
+		self._size -= 1
+		if self.is_empty():
+			self._tail = None
+		return answer
+
+	def remove_last(self):
+		if self.is_empty():
+			raise Empty('Linked list is empty')
+		if self._size == 1:
+			answer = self._head._element
+			self._head = None
+			self._tail = None
+		else:
+			answer = self._tail._element
+			current_node = self._head
+			next_node = current_node._next
+			while next_node != self._tail:
+				current_node = next_node
+				next_node = next_node._next
+			self._tail = current_node
+		self._size -= 1
+		return answer

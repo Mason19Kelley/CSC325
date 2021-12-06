8bc4ebd195336f6ca6e10ef4b865a22293ead2bf
j-w-s <69388380+j-w-s@users.noreply.github.com>
2021-12-06 10:07:37 -0600

    Update q7.py

---
+++ PS1/q7.py
@@ -0,0 +1,16 @@
+'''R-6.10 Consider what happens if the loop in the ArrayQueue. resize method at
+lines 53–55 of Code Fragment 6.7 
+
+for k in range(self._size):     # only consider existing elements
+  self._data[k] = old[k] # rather than old[walk] # intentionally shift indices
+  walk = (1 + walk) % len(old)  # use old size as modulus
+  
+
+had been implemented as:
+
+
+for k in range(self._size):
+  self._data[k] = old[k] # rather than old[walk]
+  
+  
+Give a clear explanation of what could go wrong.'''

9a928f831f2aea1672437fae471a351cb4cfa959
Jacob <57639784+Sherm1111@users.noreply.github.com>
2021-12-11 22:16:18 -0600

    Done?

--- PS1/q4.py
+++ PS1/q4.py
@@ -1,1 +1,6 @@
 #show the summation of log(i) for i=1 to N is Ω(nlog(n)) 
+Σ(from i=1 to N) (log(i)) = log(1)+log(2)+...+log(N)
+>= Σ(from i=1 to N/2) log(i)
+>= n/2 log(n/2)
+>= n/4 log(n) for n>=4
+therefore Σ(from i=1 to N) log(i) = Ω(nlog(n))

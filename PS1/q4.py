ae25f2bd005eb5f2ab9241141108b61fd6be329b
j-w-s <69388380+j-w-s@users.noreply.github.com>
2021-12-14 23:00:26 -0600

    Update q4.py
    done maybe?

--- PS1/q4.py
+++ PS1/q4.py
@@ -1,6 +1,9 @@
-#show the summation of log(i) for i=1 to N is Ω(nlog(n)) 
-Σ(from i=1 to N) (log(i)) = log(1)+log(2)+...+log(N)
->= Σ(from i=1 to N/2) log(i)
+# show the summation of log(i) for i=1 to N is Ω(nlog(n)) 
+Σ(from i=1 to N) (log(i)) = log(1) + log(2) + ... log (n/2) + ... + log(N)
+>= 0 * log(1) + 0 * log(2) + ... log(n/2) + ... + log(N)
 >= n/2 log(n/2)
->= n/4 log(n) for n>=4
-therefore Σ(from i=1 to N) log(i) = Ω(nlog(n))
+# division of logs, so:
+= n/2 (log(n) - log(2))
+= n/2 (log(n) - 1)
+= (n/2)(log(n)) - (n/2)
+# thus, Σ(from i=1 to N) (log(i)) = Ω(nlog(n))

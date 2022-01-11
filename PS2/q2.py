b6aac844f58d61665e6ae460ac6265583877cf55
Jacob <57639784+Sherm1111@users.noreply.github.com>
2022-01-10 19:02:42 -0600

    Added Question

---
+++ PS2/q2.py
@@ -0,0 +1,5 @@
+#Define the internal path length, I(T), of a tree T to be the sum of the
+#depths of all the internal positions in T. Likewise, define the external path
+#length, E(T), of a tree T to be the sum of the depths of all the external
+#positions in T. Show that if T is a proper binary tree with n positions, then
+#E(T) = I(T) +n−1.

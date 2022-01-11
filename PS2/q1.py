e9a8362ee5afc592ba4f57a5f21625607f32474e
Jacob <57639784+Sherm1111@users.noreply.github.com>
2022-01-10 18:58:50 -0600

    Added Question

---
+++ PS2/q1.py
@@ -0,0 +1,6 @@
+#Let T be an ordered tree with more than one node. Is it possible that the
+#preorder traversal of T visits the nodes in the same order as the postorder
+#traversal of T? If so, give an example; otherwise, explain why this cannot
+#occur. Likewise, is it possible that the preorder traversal of T visits the
+#nodes in the reverse order of the postorder traversal of T? If so, give an
+#example; otherwise, explain why this cannot occur.

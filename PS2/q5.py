be9a92a1f25e4a7c059933b4bd0395f71495c06f
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2022-01-10 19:50:37 -0600

    q5 base stuff added

--- PS2/q5.py
+++ PS2/q5.py
@@ -1,2 +1,12 @@
 #Illustrate the execution of the in-place heap-sort algorithm on the following
 #input sequence: (2,5,16,4,10,23,39,18,26,15).
+
+# tree-based representation of the sequence
+#                      2
+#          5                       16
+#      4          10           23      39
+#   18   26    15
+
+
+
+# First, we must heapify the sequence into a max heap

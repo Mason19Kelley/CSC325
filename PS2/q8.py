2b661f0d01c5eb8f531891a8f2ae98a9222e26a7
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2022-01-12 21:56:35 -0600

    q8 done

--- PS2/q8.py
+++ PS2/q8.py
@@ -1,6 +1,7 @@
 #9-10 At which positions of a heap might the third smallest key be stored?
 '''
-The third smallest element might be stored as one of the children of the root node. 
+The third smallest element will be stored in the 2nd or 3rd level of the tree
+representation of a heap depending on insertion order.
 '''
 
 #9-11 At which positions of a heap might the largest key be stored?

db23ecda2eccd7453273618cd5369fa3681172e6
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2022-01-12 21:55:47 -0600

    q7 done

--- PS2/q7.py
+++ PS2/q7.py
@@ -1,2 +1,13 @@
  #Implement the in-place heap-sort algorithm and experimentally determine its 
  #running time (using a similar approach as in the first assignment).
+
+
+# Turns an array into a max-heap
+def heapify(Arr, ):
+
+
+
+
+
+# turn a Max-heap into a sorted array
+def heapSort(Arr):

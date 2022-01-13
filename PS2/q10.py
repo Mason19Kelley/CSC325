770acf5fe1b68f479fda4b80a2be317ee89149a4
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2022-01-12 22:19:52 -0600

    added p to q10

---
+++ PS2/q10.py
@@ -0,0 +1,3 @@
+# Draw an example skip list S that results from performing the following
+# series of operations on the skip list shown in Figure 10.13: del S[38],
+# S[48] = x , S[24] = y , del S[55]. Record your coin flips, as well.

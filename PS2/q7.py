37f3a6f081760c0a505aee3e714c2bda3b30578d
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2022-01-12 22:07:45 -0600

    q7 half done

--- PS2/q7.py
+++ PS2/q7.py
@@ -1,10 +1,25 @@
  #Implement the in-place heap-sort algorithm and experimentally determine its 
  #running time (using a similar approach as in the first assignment).
 
+arr = [1,7,43,90,4,6]
+# Turns an array into a max heap by working from the bottom up with each sub-heap
+def heapify(Arr, Arr_len, i):
+	# set indixes for current subheap
+	larg = i
+	l_ind = 2*i+1
+	r_ind = 2*i+2
 
-# Turns an array into a max-heap
-def heapify(Arr, ):
+	# find largest value
+	if l_ind < Arr_len and A[l_ind] > A[larg]:
+		larg = l_ind
 
+	if l_ind < Arr_len and A[l_ind] > A[larg]:
+		larg = r_ind
+
+	if larg != i:
+		Arr[i], Arr[larg] = Arr[larg], Arr[i]
+		# run heapify again to find largest node between the 3
+		heapify(Arr , Arr_len, larg)
 
 
 

1112e26bab00ce6cfe30753f4a254fc4cbec4a94
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2022-01-31 11:11:49 -0600

    q6 done

--- PS3/q6.py
+++ PS3/q6.py
@@ -2,10 +2,15 @@
 # the same node values.
 # Use the following binary search tree node class:
 class tree_node:
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
+	def __init__(self, value, left=None, right=None):
+		self.value = value
+		self.left = left
+		self.right = right
+
+	def __str__(self):
+		return str(self.value)
+
+
 # In other words, you can freely use p.value, p.left, and p.right for a node p in your function to access the value,
 # left child, and right child of p.
 # If there is more than one answer, your function can return any of them.
@@ -31,16 +36,21 @@ def inorder(root, arr=[]):
 		arr.append(root.value)
 		inorder(root.right, arr)
 
-def in2avl(arr):
-	pass
 
+def in2avl(arr):
+	if len(arr) > 0:
+		mindex = (len(arr)-1) // 2
+
+		return tree_node(arr[mindex], in2avl(arr[0:mindex]), in2avl(arr[mindex + 1:len(arr)]))
 
 
 if __name__ == "__main__":
 	arr = []
 	head = tree_node(1, None, tree_node(2, None, tree_node(3, None, tree_node(4))))
 	inorder(head, arr)
-
-
-
-
+	print(arr)
+	head2 = in2avl(arr)
+	print(head2)
+	print(head2.left)
+	print(head2.right)
+	print(head2.right.right)

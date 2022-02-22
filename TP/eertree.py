695167b3f3e4fc6e691792e3193ca94993dfc173
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2022-02-21 23:03:55 -0600

    updates

--- TP/eertree.py
+++ TP/eertree.py
@@ -15,14 +15,14 @@ class Eertree(object):
         self.root_even = Node() #even length root node, or node 0
  
         # Initialize empty tree
-        self.root_odd.link = self.root_even.link = self.root_odd;
+        self.root_odd.link = self.root_even.link = self.root_odd
         self.root_odd.len = -1
         self.root_even.len = 0
         self.S = [0] # list that stores all chars in eertree
         self.maxSufT = self.root_even # maximum suffix of tree T, initially 0
 
 
-
+    # returns node that should be above the newly created node after a character is inserted
     def get_max_suffix_pal(self, startNode, a):
         node = startNode
         i = len(self.S)
@@ -33,37 +33,37 @@ class Eertree(object):
             k = node.len
         return node
  
-    def add(self, a):
-        # Start by finding maximum suffix-palindrome Q of T.
-        Q = self.get_max_suffix_pal(self.maxSufT, a)
-        # We check Q to see whether or not it has an edge to a
-        NewNode = not a in Q.edges
+    def add(self, new_char):
+        # find the node directly above the new node that will be inserted
+        prev = self.get_max_suffix_pal(self.maxSufT, new_char)
 
+        # We check prev node to see whether or not it has an edge to a
+        NewNode = not new_char in prev.edges
         if NewNode:
             # create the new node
-            P = Node()
-            self.nodes.append(P)
-            P.len = Q.len + 2
-            if P.len == 1:
+            new = Node()
+            self.nodes.append(new)
+            new.len = prev.len + 2
+            if new.len == 1:
                 # if the length of the new node is 1, we must connect it's backwards link to 0
-                P.link = self.root_even
+                new.link = self.root_even
             else:
-                # It remains to create the suffix link from P if |P|>1. Just
-                # continue traversing suffix-palindromes of T starting with the suffix
-                # link of Q.
-                P.link = self.get_max_suffix_pal(Q.link, a).edges[a]
+                #backwards link from new node to edge of prev nodes
+                new.link = self.get_max_suffix_pal(prev.link, new_char).edges[new_char]
  
-            # create the edge (Q,P)
-            Q.edges[a] = P
+            # create the edge (prev - new)
+            prev.edges[new_char] = new
  
-        #P becomes the new maxSufT
-        self.maxSufT = Q.edges[a]
+        #new becomes the new maxSufT
+        self.maxSufT = prev.edges[new_char]
  
-        #Store accumulated input string
-        self.S.append(a)
+        #add new char to stored string
+        self.S.append(new_char)
  
         return NewNode
- 
+
+
+    # prints out all sub-palindromes by traversing through the string and nodes
     def get_sub_palindromes(self, nd, nodesToHere, charsToHere, result):
         #Each node represents a palindrome, which can be reconstructed
         #by the path from the root node to each non-root node.
